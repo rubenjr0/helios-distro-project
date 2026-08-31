4041259fe70d751275b07819d5224b28a46895ad
rubenjr0 <rubenjr0@proton.me>
2026-08-31 21:48:51 +0200

    update buld script

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,12 +3,6 @@
 set -ouex pipefail
 
 # https://github.com/R-Dson/bazzite-based-cosmic-nvidia/
-dnf5 remove -y xwaylandvideobridge
-dnf5 group remove -y kde-desktop
-
-
-dnf5 clean all && \
-rm -rf /var/cache/dnf/*
 
 dnf5 -y copr enable ryanabx/cosmic-epoch
 dnf5 group install -y cosmic-desktop
